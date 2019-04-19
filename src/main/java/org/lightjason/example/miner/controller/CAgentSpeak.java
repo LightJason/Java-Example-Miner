f54d06e55cb2e4726fe347b8baf14f529f0db67d
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-04-19 06:06:20 +0200

    agentspeak controller, add chart.js

---
+++ src/main/java/org/lightjason/example/miner/controller/CAgentSpeak.java
@@ -0,0 +1,65 @@
+/*
+ * @cond LICENSE
+ * ######################################################################################
+ * # LGPL License                                                                       #
+ * #                                                                                    #
+ * # This file is part of the LightJason                                                #
+ * # Copyright (c) 2015-19, LightJason (info@lightjason.org)                            #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU Lesser General Public License as                     #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU Lesser General Public License for more details.                                #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU Lesser General Public License           #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package org.lightjason.example.miner.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+/**
+ * agentspeak rest controller
+ */
+@RestController
+@RequestMapping( "/agentspeak" )
+public final class CAgentSpeak
+{
+
+    /**
+     * returns a list with existing actions of the miner agent
+     *
+     * @return action name
+     */
+    @RequestMapping( value = "/action/miner", produces = APPLICATION_JSON_VALUE )
+    public Collection<String> mineractions()
+    {
+        return Collections.emptySet();
+    }
+
+    /**
+     * returns a list with existing actions of the world agent
+     *
+     * @return action name
+     */
+    @RequestMapping( value = "/action/world", produces = APPLICATION_JSON_VALUE )
+    public Collection<String> worldactions()
+    {
+        return Collections.emptySet();
+    }
+
+}
