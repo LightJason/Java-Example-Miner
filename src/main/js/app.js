db0a012a87bd889e12d357285c2bbf1fccbbdb24
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-04-19 16:12:59 +0200

    add webpack & react

---
+++ src/main/js/app.js
@@ -0,0 +1,16 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<span>Test</span>
+		)
+	}
+}
