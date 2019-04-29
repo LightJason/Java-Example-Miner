8b5be5c731c5c465dcf1a348f3cfe5a38af79d81
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-04-29 14:57:08 +0200

    menu

--- src/main/js/header.js
+++ src/main/js/header.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, NavbarToggler, Nav, NavItem, NavLink, Collapse, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
+// https://reactstrap.github.io/components/navbar/
 
 class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
 	render() {
 		return (
             <Navbar dark color="dark" expand="w-100">
                 <NavbarBrand><span id="title">LightJason - Miner</span></NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>Options</DropdownToggle>
+                                <DropdownMenu right>
+                                <DropdownItem>Option 1</DropdownItem>
+                                <DropdownItem>Option 2</DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>Reset</DropdownItem>
+                            </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
             </Navbar>
 		);
 	}
--- src/main/js/screen.js
+++ src/main/js/screen.js
@@ -5,6 +5,7 @@ class Screen extends React.Component {
     componentDidMount() {
         // https://tutorialzine.com/2015/06/making-your-first-html5-game-with-phaser
         // https://phaser.io/examples/v2/category/tilemaps
+        // https://www.zephyrcoding.com/phaser3-with-react/
 
         let l_game = new Phaser.Game( 800, 800, Phaser.AUTO, "phaser-container", {
             transparent: true
