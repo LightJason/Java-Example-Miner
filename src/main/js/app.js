d7ed094286317e0df956fb42021aea856acfe960
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-05-20 07:43:59 +0200

    fix modal dialog

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu from "./menu.js"
 import Content from "./content.js"
+import Editor from "./editor.js";
 
 // https://spring.io/guides/tutorials/react-and-spring-data-rest/
 
@@ -14,6 +15,7 @@ class App extends React.Component {
 	render() {
 		return (
 		    <>
+		        <Editor/>
                 <Menu/>
                 <Content/>
             </>
--- src/main/js/content.js
+++ src/main/js/content.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Screen from "./screen.js";
-import Editor from "./editor.js";
 
 /**
  * content definition with the screen
@@ -10,12 +9,9 @@ export default class Content extends React.Component {
 
 	render() {
 		return (
-		    <>
-                <Editor/>
-                <main id="content">
-                    <Screen/>
-                </main>
-            </>
+            <main id="content">
+                <Screen/>
+            </main>
 		);
 	}
 
